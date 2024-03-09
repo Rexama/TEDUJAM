f1742bdc2b5f38a0b1fbc2101928478738a76820
Rekin Barışıcı <57857241+Rexama@users.noreply.github.com>
2024-03-09 09:49:06 +0300

    added draggable

---
+++ Assets/_Code/Draggable.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        offset = gameObject.transform.position - GetMouseWorldPosition();
+        isDragging = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = GetMouseWorldPosition() + offset;
+            transform.position = newPosition;
+        }
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
