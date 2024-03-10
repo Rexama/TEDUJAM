b66d24383cf0b072b9cda434a6dc1f36a073613f
Rekin Barışıcı <57857241+Rexama@users.noreply.github.com>
2024-03-10 10:10:13 +0300

    sound manager added

---
+++ Assets/_Code/SoundManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using FMODUnity;
+
+public class SoundManager : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string fmodEventPath; // Set this in the inspector to the path of your FMOD event
+
+    private FMOD.Studio.EventInstance soundEvent;
+
+    void Start()
+    {
+        // Create an instance of the FMOD event
+        soundEvent = RuntimeManager.CreateInstance(fmodEventPath);
+    }
+
+    void Update()
+    {
+        // Example: play the sound when the user presses a key
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlaySound();
+        }
+    }
+
+    void PlaySound()
+    {
+        // Start playing the FMOD event
+        soundEvent.start();
+    }
+
+    void OnDestroy()
+    {
+        // Release the FMOD event instance when the object is destroyed
+        soundEvent.release();
+    }
+}
